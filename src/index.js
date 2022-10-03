1a2e4566d8d968217a43ae0b2fb1b71ada5d98d0
Angel Romero <mrangelrcd@gmail.com>
2022-10-03 23:03:12 +0200

    The dices, functional

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+const dado1 = document.querySelector(".button.first");
+const dado2 = document.querySelector(".button.second");
+const dado3 = document.querySelector(".button.third");
+dado1.addEventListener("click", insertDiceNumberFirst);
+dado2.addEventListener("click", insertDiceNumberSecond);
+dado3.addEventListener("click", insertDiceNumberThird);
+
 function generateRandomNumber() {
   let number = Math.random();
   number = number * 6;
   number = Math.round(number);
   return number;
 }
-function insertDiceNumber(i) {
+function insertDiceNumberFirst() {
   const diceNumberWrap = document.querySelector(".dice-container.first p");
   diceNumberWrap.textContent = generateRandomNumber();
 }
 
-const dado1 = document.querySelector(".button");
-const dado2 = document.querySelector(".button.second");
-dado1.addEventListener("click", insertDiceNumber);
-dado2.addEventListener("click", insertDiceNumber);
+function insertDiceNumberSecond() {
+  const diceNumberWrap = document.querySelector(".dice-container.second p");
+  diceNumberWrap.textContent = generateRandomNumber();
+}
+
+function insertDiceNumberThird() {
+  const diceNumberWrap = document.querySelector(".dice-container.third p");
+  diceNumberWrap.textContent = generateRandomNumber();
+}
