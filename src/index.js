ba60f3656e974f6c87297d8dd268adfa73751157
Angel Romero <mrangelrcd@gmail.com>
2022-10-03 22:47:58 +0200

    Two dices, not finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-
-const dado = document.querySelector(".button");
-dado.addEventListener("click", insertDiceNumber);
-
-function throwDice() {
+function generateRandomNumber() {
   let number = Math.random();
   number = number * 6;
   number = Math.round(number);
   return number;
 }
-function insertDiceNumber() {
-  const diceNumberWrap = document.querySelector(".dice-container p");
-  diceNumberWrap.textContent = throwDice();
+function insertDiceNumber(i) {
+  const diceNumberWrap = document.querySelector(".dice-container.first p");
+  diceNumberWrap.textContent = generateRandomNumber();
 }
+
+const dado1 = document.querySelector(".button");
+const dado2 = document.querySelector(".button.second");
+dado1.addEventListener("click", insertDiceNumber);
+dado2.addEventListener("click", insertDiceNumber);
